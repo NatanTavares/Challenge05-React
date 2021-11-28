47c28bde84fb9f6f63fc29f0469265d1e9218531
NatanTavares <nattantavares.s15@gmail.com>
2021-11-28 17:42:25 -0300

    feat(component): Implements the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import { useRouter } from 'next/router';
+
+export default function Header(): JSX.Element {
+  const router = useRouter();
+
+  return (
+    <header>
+      {/* eslint-disable-next-line */}
+      <img
+        src="/logo.svg"
+        alt="logo"
+        onClick={() => router.push(`/`, '', { shallow: true })}
+      />
+    </header>
+  );
 }
